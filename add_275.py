faf11e876501f6c89dbcd7cd8087875bb0122186
lilibei <lilibei1989@sina.com>
2017-07-18 08:27:54 +0800

    Update add_275.py

--- add_275.py
+++ add_275.py
@@ -2,30 +2,30 @@
 
 ha_275 = {}
 
-with open('275_rawVariants.SNP.vcf','r') as fd:
+with open('275_adddd_ID.vcf','r') as fd:
     for line in fd:
         if line.startswith('#'):
             continue
         else:
-            k = line.split()[0] + "_" + line.split()[1]
+            k = line.split()[2]
             ha_275[k] = line.split()[9]
 
 
-with open('minQ60_minMeanDP_1.6_maxMeanDP_25_remove_LowQual_maf0.01.recode.vcf','r') as fd:
+with open('436_raw.vcf','r') as fd:
     for line in fd:
         if line.startswith('##'):
             continue
         if line.split()[0] == "#CHROM":
             header = line.split()
-            header.append('275')
+            header.append('S275')
             line_header = "\t".join(header)
             print line_header
             continue
         line_list = line.split()
-        a = line_list[0] + "_" + line_list[1]
-        if a in ha_275.keys():
-            new_line1 = line.strip() + "\t" + ha_275[a]
-            ha_275.pop(a)
+        ID = line_list[2]
+        if ID in ha_275.keys():
+            new_line1 = line.strip() + "\t" + ha_275[ID]
+            ha_275.pop(ID)
             print new_line1
             continue
         else:
